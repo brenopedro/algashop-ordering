b3e59a244956db2d44cf0d50ae9ac93d649a2982
pinto.breno <breno.pedro.a.pinto@accenture.com>
2026-02-23 18:00:33 -0300

    testes unitários do repository

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -1,16 +1,50 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.repository;
 
+import com.algaworks.algashop.ordering.domain.model.utility.IdGenerator;
+import com.algaworks.algashop.ordering.infrastructure.persistence.entity.OrderPersistenceEntity;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@Transactional
 class OrderPersistenceEntityRepositoryIT {
 
-    @Test
-    void shouldRun() {
+    private final OrderPersistenceEntityRepository orderPersistenceEntityRepository;
 
+    @Autowired
+    public OrderPersistenceEntityRepositoryIT(OrderPersistenceEntityRepository orderPersistenceEntityRepository) {
+        this.orderPersistenceEntityRepository = orderPersistenceEntityRepository;
+    }
+
+    @Test
+    void shouldPersiste() {
+        long orderId = IdGenerator.generatiTSID().toLong();
+        OrderPersistenceEntity entity = OrderPersistenceEntity.builder()
+                .id(orderId)
+                .customerId(IdGenerator.generateTimeBasedUUID())
+                .totalItems(2)
+                .totalAmount(BigDecimal.TEN)
+                .status("DRAFT")
+                .paymentMethod("CREDIT_CARD")
+                .placedAt(OffsetDateTime.now())
+                .build();
+
+        orderPersistenceEntityRepository.saveAndFlush(entity);
+
+        assertThat(orderPersistenceEntityRepository.existsById(orderId)).isTrue();
+    }
+
+    @Test
+    void shouldCount() {
+        long count = orderPersistenceEntityRepository.count();
+        assertThat(count).isZero();
     }
 
 }
