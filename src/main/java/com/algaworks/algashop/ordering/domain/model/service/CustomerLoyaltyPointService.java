aac138340b00685d4dc72a4d41cf3663aa60c9d6
Breno Pedro <brenopedro14@hotmail.com>
2026-03-10 13:54:01 -0300

    criando anotação para services e evitando acoplamento de dependência

--- src/main/java/com/algaworks/algashop/ordering/domain/model/service/CustomerLoyaltyPointService.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/service/CustomerLoyaltyPointService.java
@@ -4,6 +4,7 @@ import com.algaworks.algashop.ordering.domain.model.entity.Customer;
 import com.algaworks.algashop.ordering.domain.model.entity.Order;
 import com.algaworks.algashop.ordering.domain.model.exception.CantAddLoyaltyPointsOrderIsNotReadyException;
 import com.algaworks.algashop.ordering.domain.model.exception.OrderDoesNotBelongsToCustomerException;
+import com.algaworks.algashop.ordering.domain.model.utility.DomainService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.LoyaltyPoints;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Money;
 
@@ -12,6 +13,7 @@ import java.util.Objects;
 import static com.algaworks.algashop.ordering.domain.model.exception.ErrorMessages.VALIDATION_ERROR_CUSTOMER_IS_NULL;
 import static com.algaworks.algashop.ordering.domain.model.exception.ErrorMessages.VALIDATION_ERROR_ORDER_IS_NULL;
 
+@DomainService
 public class CustomerLoyaltyPointService {
 
     private static final LoyaltyPoints basePoints = new LoyaltyPoints(5);
