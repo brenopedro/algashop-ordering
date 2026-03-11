bfbcd1cacd051df4cef7ed34c6f9b4c8ee6b4bc9
Breno Pedro <brenopedro14@hotmail.com>
2026-03-11 13:32:41 -0300

    criado integração com o serviço de mock

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/FixedOriginAddressService.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/fake/FixedOriginAddressService.java
@@ -3,12 +3,14 @@ package com.algaworks.algashop.ordering.infrastructure.fake;
 import com.algaworks.algashop.ordering.domain.model.service.OriginAddressService;
 import com.algaworks.algashop.ordering.domain.model.valueobject.Address;
 import com.algaworks.algashop.ordering.domain.model.valueobject.ZipCode;
+import org.springframework.stereotype.Component;
 
+@Component
 public class FixedOriginAddressService implements OriginAddressService {
     @Override
     public Address originAddress() {
         return Address.builder()
-                .state("Bourbon Street")
+                .street("Bourbon Street")
                 .number("1134")
                 .neighborhood("North Ville")
                 .city("York")
