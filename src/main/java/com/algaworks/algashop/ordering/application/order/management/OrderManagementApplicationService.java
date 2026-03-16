99dc75ac175e30569ea5b6405d1c0ef06ed633b7
Breno Pedro <brenopedro14@hotmail.com>
2026-03-16 10:28:28 -0300

    feat: criado serviço de order management

---
+++ src/main/java/com/algaworks/algashop/ordering/application/order/management/OrderManagementApplicationService.java
@@ -0,0 +1,42 @@
+package com.algaworks.algashop.ordering.application.order.management;
+
+import com.algaworks.algashop.ordering.domain.model.order.Order;
+import com.algaworks.algashop.ordering.domain.model.order.OrderId;
+import com.algaworks.algashop.ordering.domain.model.order.OrderNotFoundException;
+import com.algaworks.algashop.ordering.domain.model.order.Orders;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Service
+@RequiredArgsConstructor
+public class OrderManagementApplicationService {
+
+    private final Orders  orders;
+
+    @Transactional
+    public void cancel(Long rawOrderId) {
+        Objects.requireNonNull(rawOrderId);
+        Order order = orders.ofId(new OrderId(rawOrderId)).orElseThrow(OrderNotFoundException::new);
+        order.cancel();
+        orders.add(order);
+    }
+
+    @Transactional
+    public void markAsPaid(Long rawOrderId) {
+        Objects.requireNonNull(rawOrderId);
+        Order order = orders.ofId(new OrderId(rawOrderId)).orElseThrow(OrderNotFoundException::new);
+        order.markAsPaid();
+        orders.add(order);
+    }
+
+    @Transactional
+    public void markAsReady(Long rawOrderId) {
+        Objects.requireNonNull(rawOrderId);
+        Order order = orders.ofId(new OrderId(rawOrderId)).orElseThrow(OrderNotFoundException::new);
+        order.markAsReady();
+        orders.add(order);
+    }
+}
