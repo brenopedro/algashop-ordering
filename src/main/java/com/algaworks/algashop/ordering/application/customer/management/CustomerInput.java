26e97d9ff595ba9dfd7df74bc9f0abb1c74e87a8
Breno Pinto <brenopedro14@hotmail.com>
2026-04-22 17:43:30 -0300

    feat: validação de campos do customerInput

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerInput.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerInput.java
@@ -1,6 +1,12 @@
 package com.algaworks.algashop.ordering.application.customer.management;
 
 import com.algaworks.algashop.ordering.application.commons.AddressData;
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,12 +20,30 @@ import java.time.LocalDate;
 @AllArgsConstructor
 public class CustomerInput {
 
+    @NotBlank
     private String firstName;
+
+    @NotBlank
     private String lastName;
+
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
     private String phone;
+
+    @NotBlank
     private String document;
+
+    @Past
+    @NotNull
     private LocalDate birthDate;
+
+    @NotNull
     private Boolean promotionNotificationsAllowed;
+
+    @Valid
+    @NotNull
     private AddressData address;
 }
--- src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
+++ src/main/java/com/algaworks/algashop/ordering/presentation/CustomerController.java
@@ -1,42 +1,34 @@
 package com.algaworks.algashop.ordering.presentation;
 
+import java.util.UUID;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.OffsetDateTime;
-import java.util.UUID;
-
-import org.springframework.http.HttpStatus;
-
-import com.algaworks.algashop.ordering.application.commons.AddressData;
 import com.algaworks.algashop.ordering.application.customer.management.CustomerInput;
+import com.algaworks.algashop.ordering.application.customer.management.CustomerManagementApplicationService;
 import com.algaworks.algashop.ordering.application.customer.query.CustomerOutput;
+import com.algaworks.algashop.ordering.application.customer.query.CustomerQueryService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
 
-  @PostMapping
-  @ResponseStatus(HttpStatus.CREATED)
-  public CustomerOutput createCustomer(@RequestBody CustomerInput input) {
-    AddressData address = input.getAddress();
+    private final CustomerManagementApplicationService customerManagementApplicationService;
+    private final CustomerQueryService customerQueryService;
 
-    return CustomerOutput.builder()
-        .id(UUID.randomUUID())
-        .firstName(input.getFirstName())
-        .lastName(input.getLastName())
-        .phone(input.getPhone())
-        .email(input.getEmail())
-        .birthDate(input.getBirthDate())
-        .document(input.getDocument())
-        .promotionNotificationsAllowed(input.getPromotionNotificationsAllowed())
-        .registeredAt(OffsetDateTime.now())
-        .archived(false)
-        .loyaltyPoints(0)
-        .address(address)
-        .build();
-  }
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomerOutput createCustomer(@RequestBody @Valid CustomerInput input) {
+        UUID customerId = customerManagementApplicationService.create(input);
+        return customerQueryService.findById(customerId);
+    }
 }
