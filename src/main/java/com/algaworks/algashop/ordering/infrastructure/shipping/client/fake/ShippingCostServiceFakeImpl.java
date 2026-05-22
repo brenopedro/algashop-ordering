2ff9b78bc38ed0571a58be23bde7385223ed3ca2
Breno Pinto <brenopedro14@hotmail.com>
2026-05-22 11:55:26 -0300

    feat: adicionado o wiremock no teste de shipping cost

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/shipping/client/fake/ShippingCostServiceFakeImpl.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/shipping/client/fake/ShippingCostServiceFakeImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 
 @Component
-@ConditionalOnProperty(name = "algashop.integrations.shipping.provider", havingValue = "FAKE")
+//@ConditionalOnProperty(name = "algashop.integrations.shipping.provider", havingValue = "FAKE")
 public class ShippingCostServiceFakeImpl implements ShippingCostService {
 
 
--- src/test/java/com/algaworks/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/model/order/shipping/ShippingCostServiceIT.java
@@ -1,14 +1,17 @@
 package com.algaworks.algashop.ordering.domain.model.order.shipping;
 
 import com.algaworks.algashop.ordering.domain.model.commons.ZipCode;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import static com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService.CalculationRequest;
 import static com.algaworks.algashop.ordering.domain.model.order.shipping.ShippingCostService.CalculationResult;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class ShippingCostServiceIT {
@@ -18,6 +21,27 @@ class ShippingCostServiceIT {
 
     @Autowired
     private OriginAddressService originAddressService;
+
+    private WireMockServer wireMockRapidex;
+
+    @BeforeEach
+    public void setup() {
+        initWireMock();
+    }
+
+    @AfterEach
+    public void clean() {
+        wireMockRapidex.stop();
+    }
+
+    private void initWireMock() {
+        wireMockRapidex = new WireMockServer(options()
+                .port(8780)
+                .usingFilesUnderDirectory("src/test/resources/wiremock/rapidex"));
+
+        wireMockRapidex.start();
+    }
+
 
     @Test
     void shouldCalculate() {
