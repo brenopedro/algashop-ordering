11d5e0cf202ff1fb15a77df69c6de78a1d4a6405
Breno Pedro Assis Tenório Pinto <brenopedro14@hotmail.com>
2026-08-13 21:48:23 -0300

    feat: implementação do circuit breaker

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/adapters/out/web/product/client/http/ResilientProductCatalogAPIClient.java
@@ -2,11 +2,13 @@ package com.algaworks.algashop.ordering.infrastructure.adapters.out.web.product.
 
 import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.excpetionhandler.BadGatewayException;
 import com.algaworks.algashop.ordering.infrastructure.adapters.in.web.excpetionhandler.GatewayTimeoutException;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.client.circuitbreaker.NoFallbackAvailableException;
+import org.springframework.core.retry.RetryException;
 import org.springframework.resilience.annotation.ConcurrencyLimit;
-import org.springframework.resilience.annotation.Retryable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.HttpServerErrorException;
@@ -19,25 +21,47 @@ import java.util.UUID;
 
 @Slf4j
 @Component
-@RequiredArgsConstructor
 public class ResilientProductCatalogAPIClient {
 
     private final ProductCatalogAPIClient productCatalogAPIClient;
+    private final CircuitBreaker circuitBreaker;
 
-    @Retryable(
-            maxRetries = 3,
-            delayString = "3s",
-            multiplier = 2,
-            includes = {GatewayTimeoutException.class, BadGatewayException.ServerErrorException.class}
-    )
+    public ResilientProductCatalogAPIClient(ProductCatalogAPIClient productCatalogAPIClient,
+                                            CircuitBreakerFactory circuitBreakerFactory) {
+        this.productCatalogAPIClient = productCatalogAPIClient;
+        this.circuitBreaker = circuitBreakerFactory.create("productCatalogCB");
+    }
+
     @Cacheable(cacheNames = "algashop:product-catalog-api:v1", key = "#productId")
     @ConcurrencyLimit(10)
     public Optional<ProductResponse> getById(UUID  productId) {
 
-        log.info("Loading product {}", productId);
+        log.info("Trying to load product {}", productId);
         try {
-            return Optional.ofNullable(productCatalogAPIClient.getById(productId));
-        } catch (HttpClientErrorException.NotFound ex) {
+            return loadProduct(productId);
+        } catch (NoFallbackAvailableException ex) {
+            throw unwrapException(ex);
+        }
+    }
+
+    private RuntimeException unwrapException(NoFallbackAvailableException ex) {
+        if (ex.getCause() instanceof RetryException re) {
+            if (re.getCause() instanceof GatewayTimeoutException gte)
+                return gte;
+            if (re.getCause() instanceof BadGatewayException bge)
+                return bge;
+        }
+        return ex;
+    }
+
+    private Optional<ProductResponse> loadProduct(UUID productId) {
+        try {
+            log.info("Loading product {}", productId);
+            return circuitBreaker.run(() -> Optional.ofNullable(productCatalogAPIClient.getById(productId)));
+        } catch (HttpClientErrorException ex) {
+            if (!(ex instanceof HttpClientErrorException.NotFound))
+                log.error("Client HTTP error when loading product {} ", productId, ex);
+
             return Optional.empty();
         } catch (RestClientException ex) {
             throw translateException(ex);
--- src/main/java/com/algaworks/algashop/ordering/infrastructure/config/resilince/SpringResilienceConfig.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/config/resilince/SpringResilienceConfig.java
@@ -1,4 +1,4 @@
-package com.algaworks.algashop.ordering.infrastructure.config.spring;
+package com.algaworks.algashop.ordering.infrastructure.config.resilince;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.resilience.annotation.EnableResilientMethods;
